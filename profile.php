357effbd5c495985b36a643b4a8d3fcb53ac11c9
jamesacuan <jec@sacuan.com>
2018-09-28 11:17:25 +0800

    profile changes

--- profile.php
+++ profile.php
@@ -1,6 +1,9 @@
 <?php
-// core configuration
 include_once "config/core.php";
+include_once "config/database.php";
+
+$database = new Database();
+$db = $database->getConnection();
 
 // set page title
 $page_title="View Profile";
@@ -8,7 +11,12 @@ $page_title="View Profile";
 // include login checker
 $require_login=true;
 include_once "login_check.php";
-include 'template-header.php';
+include 'template/header.php';
 ?>
 
-<?php include 'template-footer.php' ?>
+<?php
+echo "<h2>Title</h2>";
+?>
+
+
+<?php include 'template/footer.php' ?>
